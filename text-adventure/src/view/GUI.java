9e4a6df2b42f1c38c0f84d443a494ffa275fc6f9
mnickels <mike@tier42.com>
2016-02-05 15:48:36 -0800

    Some GUI code

--- text-adventure/src/view/GUI.java
+++ text-adventure/src/view/GUI.java
@@ -10,8 +10,31 @@
  */
 package view;
 
-public class GUI {
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
+/**
+ * GUI for the program.
+ * 
+ * @author Mike Nickels
+ * @version 0.1
+ */
+public class GUI extends JFrame {
+	/**
+	 * Default ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final String TITLE = "Steampunk Text-Based Adventure Game";
 	
+	public GUI() {
+		
+	}
+	
+	private class DisplayedText extends JScrollPane {
+		
+		
+		
+	}
 	
 }
